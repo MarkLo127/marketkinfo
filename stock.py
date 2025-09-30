a32cce8b253d9c02db1a93e7660314f1dcac849e
MarkLo <yao920127@gmail.com>
2025-09-30 23:15:35 +0800

    Update stock.py

--- stock.py
+++ stock.py
@@ -93,7 +93,7 @@ def app():
                 company = cominfo(symbol)
     
                 # 獲取地址資訊
-                address = company.com_info["address1"]
+                address = company.com_info["address"]
                 city = company.com_info["city"]
                 country = company.com_info["country"]
     
